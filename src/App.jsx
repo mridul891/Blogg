2dde40335e6e7bc5252e0385d22da9b5cc3fccfd
mridul891 <58576921+mridul891@users.noreply.github.com>
2023-10-11 06:15:54 +0530

    use of react-redux for the state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./component";
+import { Outlet } from "react-router-dom";
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
   return (
-    <>
-      <h1>A blog With Appwrite </h1>
-    </>
-  )
+    !loading ? <div className="min-h-screen flex flex-wrap content-between">
+      <div className="w-full block">
+        <Header/>
+        <main>
+        Todo:{/*<Outlet/>*/}
+        </main>
+        <Footer/>
+      </div>
+    </div>:null
+  );
 }
 
-export default App
+export default App;
