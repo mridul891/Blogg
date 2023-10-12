2faf794082b59985529d3f1f0a35d338468a9519
mridul891 <58576921+mridul891@users.noreply.github.com>
2023-10-12 06:25:16 +0530

    creation of Production grade react components

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
-
+import React from "react";
+import { nanoid } from "@reduxjs/toolkit";
+import { Container, LogoutBtn } from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 function Header() {
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/Login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/Signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
   return (
-    <div>
-      header
-    </div>
-  )
+    <header className=" py-3 shadow bg-gray-500">
+      <Container>
+        <nav>
+          <div>
+            <Link to="\">logo</Link>
+          </div>
+          <ul className="">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={nanoid}>
+                  <button onClick={() => navigate(item.slug)}>
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
