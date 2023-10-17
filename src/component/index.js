7084bb9613f9466e1397f64d0c4b2360d3d7ad55
mridul891 <58576921+mridul891@users.noreply.github.com>
2023-10-17 06:19:25 +0530

    Creation of Rte and PostForm component

--- src/component/index.js
+++ src/component/index.js
@@ -2,9 +2,11 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from './container/Container';
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
 export {
     Header,
     Footer,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
