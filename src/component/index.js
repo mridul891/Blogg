6fc5288e8820a36842157a40c5598dfc9f52abf1
mridul891 <58576921+mridul891@users.noreply.github.com>
2023-10-18 06:20:53 +0530

    changes in the postForm

--- src/component/index.js
+++ src/component/index.js
@@ -3,10 +3,18 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container';
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Button from './Button';
+import Input from './Input';
+import RTE from "./RTE";
+import Select from './Select'
 export {
     Header,
     Footer,
     Container,
     LogoutBtn,
-    RTE
+    RTE,
+    Button,
+    Input,
+    RTE,
+    Select,
 }
